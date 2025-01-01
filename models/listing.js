e9e777df3e7e871f6dde3596767a6863cd23e4b0
Dhruv <dhruvthakral26@gmail.com>
2025-01-01 22:08:00 +0530

    ..

--- models/listing.js
+++ models/listing.js
@@ -2,23 +2,24 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title:{
-        type:String,
-        required:true
-    },
-    description: String,
-    image: {
-      type: String,
-      default: "https://plus.unsplash.com/premium_photo-1675826638532-09d5c136dd7a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      set: (v) => v==="" ? "https://plus.unsplash.com/premium_photo-1675826638532-09d5c136dd7a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,  
-    },
-    price: Number,
-    location: String,
-    country: String,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
 });
 
-
 const Listing = mongoose.model("Listing", listingSchema);
-
-// Exporting it to app.js
 module.exports = Listing;
